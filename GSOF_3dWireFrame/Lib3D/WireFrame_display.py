105a87334f8aff618246003e4e9c0e5bea676ddf
guy.soffer <Guy.Soffer@stratolaunch.com>
2026-07-06 09:11:53 -0700

    Add clip implementation for far distance aswell.

--- GSOF_3dWireFrame/Lib3D/WireFrame_display.py
+++ GSOF_3dWireFrame/Lib3D/WireFrame_display.py
@@ -24,26 +24,39 @@ class WireFrame():
         for line in obj.getLines():
             p1, p2 = line.points
             #p1[2], p2[2] = -p1[2], -p2[2]
-            z0 = -self.minViewZ
-            if (p1[2] < z0) or (p2[2] < z0):
-                ### At least one end of the line is infront of the camera
-                self.clip(p1,p2)
-                p1 = self.camera(p1)
-                p2 = self.camera(p2)
-                lcolor = line.color if color == None else color
-                self.drawLine( lcolor, p1, p2 )
+            zmin, zmax = -self.minViewZ, -self.maxViewZ
+            if (p1[2] > zmax) or (p2[2] > zmax):
+                ### Both end are in view distance
+                if (p1[2] < zmin) or (p2[2] < zmin):
+                    ### At least one end of the line is infront of the camera
+                    self.clip(p1,p2)
+                    p1 = self.camera(p1)
+                    p2 = self.camera(p2)
+                    lcolor = line.color if color == None else color
+                    self.drawLine( lcolor, p1, p2 )
 
     def clip(self, p1, p2) -> None:
-        z0, z1, z2 = -self.minViewZ, p1[2], p2[2]
+        zmin, zmax, z1, z2 = -self.minViewZ, -self.maxViewZ, p1[2], p2[2]
         x1, x2 = p1[0], p2[0]
         y1, y2 = p1[1], p2[1]
         if z1 != z2:
             Ax, Bx = _findLine(z1,x1,z2,x2)
             Ay, By = _findLine(z1,y1,z2,y2)
-            if z1 > z0:
-                p1[0], p1[1], p1[2] = calcLine(Ax, Bx, z0), calcLine(Ay, By, z0), z0
-            elif z2 > z0:
-                p2[0], p2[1], p2[2] = calcLine(Ax, Bx, z0), calcLine(Ay, By, z0), z0
+            if z1 > zmin:
+                p1[0], p1[1], p1[2] = calcLine(Ax, Bx, zmin), calcLine(Ay, By, zmin), zmin
+                if z2 < zmax:
+                    p2[0], p2[1], p2[2] = calcLine(Ax, Bx, zmax), calcLine(Ay, By, zmax), zmax
+
+            elif z2 > zmin:
+                p2[0], p2[1], p2[2] = calcLine(Ax, Bx, zmin), calcLine(Ay, By, zmin), zmin
+                if z1 < zmax:
+                   p1[0], p1[1], p1[2] = calcLine(Ax, Bx, zmax), calcLine(Ay, By, zmax), zmax
+
+            else:
+                if z2 < zmax:
+                    p2[0], p2[1], p2[2] = calcLine(Ax, Bx, zmax), calcLine(Ay, By, zmax), zmax
+                if z1 < zmax:
+                    p1[0], p1[1], p1[2] = calcLine(Ax, Bx, zmax), calcLine(Ay, By, zmax), zmax
 
     def camera(self, point) -> list:
         """ Perspective projection <https://en.wikipedia.org/wiki/3D_projection> """
